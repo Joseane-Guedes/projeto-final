10a11036779d83a2d334ae440c0be4e2672de42b
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-11-30 17:23:41 -0300

    Test: Add testes instancias

--- socialize/aplicativo/aplicativo.test.js
+++ socialize/aplicativo/aplicativo.test.js
@@ -10,19 +10,30 @@ describe('Aplicativo', () => {
     });
 
     describe('adicionarUsuario', () => {
-        test('deve adicionar um novo usuário ao aplicativo', () => {
+        test('Deve adicionar um novo usuário ao aplicativo', () => {
             aplicativo.adicionarUsuario('Stella Luna');
             expect(Aplicativo.usuarios.length).toBe(1);
+        });
+
+        test('Deve criar uma instância de Usuario ao adicionar usuário', () => {
+            const novoUsuario = aplicativo.adicionarUsuario('Josie');
+            expect(novoUsuario).toBeInstanceOf(Usuario);
         });
     });
 
     describe('adicionarDesafio', () => {
-        test('deve adicionar um novo desafio à lista de desafios do aplicativo', () => {
+        test('Deve adicionar um novo desafio à lista de desafios do aplicativo', () => {
             aplicativo.adicionarDesafio('Visite uma amizade', 20, 'Semanal');
             expect(Aplicativo.desafios.length).toBe(1);
             expect(Aplicativo.desafios[0].nome).toBe('Visite uma amizade');
             expect(Aplicativo.desafios[0].pontos).toBe(20);
             expect(Aplicativo.desafios[0].categoria).toBe('Semanal');
         });
+
+        test('Deve criar uma instância de Desafio ao adicionar desafio', () => {
+            const novoDesafio = aplicativo.adicionarDesafio('Faça uma nova amizade', 30, 'semanal');
+            expect(novoDesafio).toBeInstanceOf(Desafio);
+        });
     });
+
 });
